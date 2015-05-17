b9825c3fa3ae7f9a2e332b951a512169ffda8158
syunkitada <syun.kitada@gmail.com>
2015-05-17 20:53:22 +0900

    updated

--- simple.py
+++ simple.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-
+import re
 from fabkit import env, Service, Package
 
 
@@ -9,23 +9,30 @@ class SimpleBase():
         self.key = 'base'
         self.data = {}
         self.services = []
-        self.packages = []
+        self.packages = {}
 
     def get_init_data(self):
         if not hasattr(self, 'is_init') or self.is_init is not True:
             if self.data_key in env.cluster:
                 self.data.update(env.cluster[self.data_key])
-
+              
+            node_os = env.node['os']
             if hasattr(self, 'services'):
                 services = []
-                for service in self.services:
-                    services.append(Service(service))
+                for os_pattern, service_names in self.services.items():
+                    if re.match(os_pattern, node_os):
+                        for service_name in service_names:
+                            services.append(Service(service_name))
+                        break
                 self.services = services
 
             if hasattr(self, 'packages'):
                 packages = []
-                for package in self.packages:
-                    packages.append(Package(package))
+                for os_pattern, package_names in self.packages.items():
+                    if re.match(os_pattern, node_os):
+                        for package_name in package_names:
+                            packages.append(Package(package_name))
+                        break
                 self.packages = packages
 
             self.init_data()
